55b5367df60127db9d8b7caa00c0ccd29aadfb9e
ilavriv <lavriv92@gmail.com>
2016-09-20 09:52:31 +0300

    Added roadmap page

--- pllug/web/home/urls.py
+++ pllug/web/home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import IndexView, RoadmapView
+from .views import IndexView, RoadmapView, ContactView
 
 urlpatterns = [
+    url(r'^roadmap/', RoadmapView.as_view(), name='roadmap'),
+    url(r'^contact/', ContactView.as_view(), name='contact'),
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^roadmap/$', RoadmapView.as_view(), name="roadmap")
 ]
