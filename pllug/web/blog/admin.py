aa77c1ae71e42a9267d7fa0e91638aaabdc29b0b
Ivan Lavriv <lavriv92@gmail.com>
2016-10-21 21:54:41 +0300

    Updated static files

--- pllug/web/blog/admin.py
+++ pllug/web/blog/admin.py
@@ -3,7 +3,9 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('author', 'title', 'slug', 'content', 'created_at', 'updated_at')
+    list_display = (
+        'author', 'title', 'slug', 'content', 'created_at', 'updated_at'
+    )
     list_filter = ('author', 'created_at', 'updated_at')
 
 
